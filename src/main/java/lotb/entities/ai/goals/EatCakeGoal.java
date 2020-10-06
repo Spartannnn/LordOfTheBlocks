aac9205ceb96ab61373bb4ece082eb14c967b6a3
BuildTools <unconfigured@null.spigotmc.org>
2020-10-06 19:05:07 +0200

    Fixed EatCakeGoal for mouse entity

--- src/main/java/lotb/entities/ai/goals/EatCakeGoal.java
+++ src/main/java/lotb/entities/ai/goals/EatCakeGoal.java
@@ -1,118 +1,95 @@
 package lotb.entities.ai.goals;
 
-import java.util.EnumSet;
-
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import lotb.entities.Mouse;
 import lotb.registries.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.CakeBlock;
-import net.minecraft.entity.MobEntity;
-import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.goal.MoveToBlockGoal;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.world.IWorldReader;
 
-public class EatCakeGoal extends Goal {
-	protected static final Logger LOGGER = LogManager.getLogger();
-	private final MobEntity eaterEntity;
-	private final World entityWorld;
-	private int eatingtimer;
-	private int hunger;
-	private boolean contExcec;
-	private BlockPos cakePos;
-	
-	//constructer and cake utilitie
-	public EatCakeGoal(MobEntity grassEaterEntityIn) {
-		eaterEntity = grassEaterEntityIn;
-		entityWorld = grassEaterEntityIn.world;
-		setMutexFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK, Goal.Flag.JUMP));
-	}
-	
-	public boolean isCake(Block block) {
-		return(block == Blocks.CAKE || block == ModBlocks.BEEF_PIE.get() || block == ModBlocks.PORK_PIE.get() || block == ModBlocks.CHICKEN_PIE.get() || block == ModBlocks.MUTTON_PIE.get() || block == ModBlocks.RABBIT_PIE.get() || block == ModBlocks.VENISON_PIE.get() || block == ModBlocks.SALMON_PIE.get() || block == ModBlocks.FISH_PIE.get());
-	}
-	
-	public boolean findCake() {
-		BlockPos entitypos = new BlockPos(eaterEntity);
-		/*for (BlockPos pos :BlockPos.getAllInBoxMutable(entitypos.add(-40, -40, -40), entitypos.add(40, 40, 40))){
-            if( isCake(entityWorld.getBlockState(pos).getBlock()) ){
-                cakePos = new BlockPos(pos.getX(),pos.getY(),pos.getZ()).toImmutable();break;}}*/
-		BlockPos.getAllInBox(entitypos.add(-10, -10, -10), entitypos.add(10, 10, 10)).forEach(pos ->
-        {
-            if(isCake(entityWorld.getBlockState(pos).getBlock()))
-            {
-                cakePos=pos.toImmutable();
+public class EatCakeGoal extends MoveToBlockGoal {
+
+    private final Mouse mouse;
+    private int val;
+    private int val2;
+    private int hunger;
+    private int eatingTimer;
+
+    public EatCakeGoal(Mouse mouse) {
+        super(mouse, 1, 10);
+        this.mouse = mouse;
+        this.hunger = 40; //Change this value to the max value
+    }
+
+    @Override
+    public boolean shouldMove() {
+        return this.timeoutCounter % 100 == 0 && this.hunger <= 0;
+    }
+
+    public double getTargetDistanceSq() {
+        return 2.0D;
+    }
+
+    @Override
+    protected boolean shouldMoveTo(IWorldReader worldIn, BlockPos pos) {
+        BlockState state = worldIn.getBlockState(pos);
+        if (this.hunger <= 0)
+            return isCake(state.getBlock());
+        return false;
+    }
+
+    public boolean isCake(Block block) {
+        return (block == Blocks.CAKE || block == ModBlocks.BEEF_PIE.get() || block == ModBlocks.PORK_PIE.get() || block == ModBlocks.CHICKEN_PIE.get() || block == ModBlocks.MUTTON_PIE.get() || block == ModBlocks.RABBIT_PIE.get() || block == ModBlocks.VENISON_PIE.get() || block == ModBlocks.SALMON_PIE.get() || block == ModBlocks.FISH_PIE.get());
+    }
+
+    @Override
+    public void startExecuting() {
+        this.eatingTimer = 40; //Eating timer
+        super.startExecuting();
+    }
+
+    @Override
+    public void tick() {
+        if (this.getIsAboveDestination()) {
+            if (this.val >= 40) { //Don't change these values
+                this.eatCake();
+            } else {
+                ++this.val;
             }
-        });
-		LOGGER.debug("cake found"+cakePos);
-		return cakePos == null;
-	}
-	/**-----------------------EXCECUTION FUNCTIONS-----------------------*/
-	@Override
-	public boolean shouldExecute() {
-		hunger = Math.min(0, hunger--);
-		if (hunger == 0) {
-			return findCake();
-		}
-		return false;
-	}
-	
-	@Override
-	public void startExecuting() {
-		eatingtimer = 40;
-		contExcec = true;
-		entityWorld.setEntityState(eaterEntity, (byte)10);
-		eaterEntity.getNavigator().clearPath();
-	}
-	
-	@Override 
-	public void tick() {
-		LOGGER.debug("cake? "+cakePos+(cakePos==null));
-		if (cakePos != null) {
-			LOGGER.debug("cake found?");
-			BlockState potentialCake = entityWorld.getBlockState(cakePos);
-			if(!cakePos.withinDistance(eaterEntity.getPositionVec(), 1)){
-				eaterEntity.getNavigator().tryMoveToXYZ(cakePos.getX() + 0.5, cakePos.getY(), cakePos.getZ() + 0.5, 1);
-			}else if( isCake(potentialCake.getBlock())) {
-				eatingtimer = Math.max(0, eatingtimer - 1);
-				if (eatingtimer == 4) {
-					if (net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(entityWorld, eaterEntity)) {
-						int cakeSlices = potentialCake.get(CakeBlock.BITES);
-						if (cakeSlices <6)
-							entityWorld.setBlockState(cakePos, potentialCake.with(CakeBlock.BITES, Integer.valueOf(cakeSlices + 1)), 3);
-						else
-							entityWorld.removeBlock(cakePos, false);
-					}
-					eaterEntity.playSound(SoundEvents.ENTITY_GENERIC_EAT, 1, 1);
-					entityWorld.addParticle(ParticleTypes.BUBBLE, (double)cakePos.getX(), (double)cakePos.getY(), (double)cakePos.getZ(), 0.0D, 0.0D, 0.0D);
-					hunger += 20;
-				}
-			}
-		}else {
-			LOGGER.debug("cake gone?");
-			contExcec = false;
-		}
-	}
-	
-	
-	
-	
-	/**-----------------------TIMER STUFF-----------------------*/
-	@Override
-	public boolean shouldContinueExecuting() {
-		return eatingtimer > 0 && contExcec;
-	}
-	public int getEatingGrassTimer() {
-		return eatingtimer;
-	}
-	@Override
-	public void resetTask() {
-		eatingtimer = 0;
-	}
+            if (val2 > 100 /*Change this value for the time when the hunger should be removed in ticks (100=5sec)*/ && this.hunger > 0) {
+                this.hunger -= 5; //Change this value for the decrease amount
+                this.val2 = 0;
+            } else {
+                ++this.val2;
+            }
+        }
+
+        super.tick();
+    }
+
+    private void eatCake() {
+        if (net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.mouse.world, mouse)) {
+            BlockState state = this.mouse.world.getBlockState(this.destinationBlock);
+            if (isCake(state.getBlock())) {
+                eatingTimer = Math.max(0, eatingTimer - 1);
+                if (eatingTimer == 4) {
+                    int cakeSlice = state.get(CakeBlock.BITES);
+                    if (cakeSlice < 6)
+                        mouse.world.setBlockState(destinationBlock, state.with(CakeBlock.BITES, cakeSlice + 1), 3);
+                    else
+                        mouse.world.removeBlock(destinationBlock, false);
+                    this.hunger += 20; //Change this value for the increase amount
+                }
+                mouse.playSound(SoundEvents.ENTITY_GENERIC_EAT, 1, 1);
+                if (mouse.world.isRemote)
+                    mouse.world.addParticle(ParticleTypes.BUBBLE, destinationBlock.getX(), destinationBlock.getY(), destinationBlock.getZ(), 0.0D, 0.0D, 0.0D);
+                this.val = 0;
+            }
+        }
+    }
 }
